efe59c6c6c38c4f247682d530633de257545adef
pirikli <pirikliartan@gmail.com>
2025-04-29 02:50:28 +0330

    Reenable Disqus Comment

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1745881268',resources: [
+    cacheName: 'chirpy-1745882411',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

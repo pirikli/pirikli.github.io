183ecd8dcd3bc3a3ac120a52bfa17d025dad162e
pirikli <pirikliartan@gmail.com>
2025-04-29 02:33:40 +0330

    Reenable Disqus Comment

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741369965',resources: [
+    cacheName: 'chirpy-1745881268',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
